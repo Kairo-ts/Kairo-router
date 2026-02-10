81dfb2b7316bfded3a76ab169fffaf665d6756bc
shizuku86 <shizuku86710@gmail.com>
2026-02-10 17:11:35 +0900

    Fix requiredAddons property name typo

--- lib/constants/properties.d.ts
+++ lib/constants/properties.d.ts
@@ -5,34 +5,21 @@ export type SemVer = {
     readonly prerelease?: string;
     readonly build?: string;
 };
-export type UUID = string;
 export type EngineVersion = [number, number, number];
 export type ManifestDependency = {
-    readonly module_name: string;
+    readonly module_name: "@minecraft/server" | "@minecraft/server-ui";
     readonly version: string;
-};
-export type ScriptModule = {
-    readonly type: "script";
-    readonly language: "javascript";
-    readonly entry: string;
-    readonly version: "header.version" | string;
-    readonly uuid: UUID;
-};
-export type ResourcePack = {
-    readonly name: string;
-    readonly description: string;
-    readonly uuid: UUID;
-    readonly module_uuid: UUID;
 };
 export type AddonHeader = {
     readonly name: string;
     readonly description: string;
     readonly version: SemVer;
     readonly min_engine_version: EngineVersion;
-    readonly uuid: UUID;
 };
 export type AddonMetadata = {
     readonly authors: string[];
+    readonly url: string;
+    readonly license: string;
 };
 export type RequiredAddons = {
     readonly [addonId: string]: string;
@@ -42,8 +29,6 @@ export type KairoAddonProperties = {
     readonly id: string;
     readonly metadata: AddonMetadata;
     readonly header: AddonHeader;
-    readonly resourcepack: ResourcePack;
-    readonly modules: ScriptModule[];
     readonly dependencies: ManifestDependency[];
     readonly requiredAddons: RequiredAddons;
     readonly tags: SupportedTag[];
--- src/constants/properties.ts
+++ src/constants/properties.ts
@@ -2,31 +2,15 @@ export type SemVer = {
   readonly major: number;
   readonly minor: number;
   readonly patch: number;
-  readonly prerelease?: string; // "preview.3" / "rc.1"
-  readonly build?: string; // "abc123" (commit)
+  readonly prerelease?: string;
+  readonly build?: string;
 };
 
-export type UUID = string;
 export type EngineVersion = [number, number, number];
 
 export type ManifestDependency = {
-  readonly module_name: string;
+  readonly module_name: "@minecraft/server" | "@minecraft/server-ui";
   readonly version: string;
-};
-
-export type ScriptModule = {
-  readonly type: "script";
-  readonly language: "javascript";
-  readonly entry: string;
-  readonly version: "header.version" | string;
-  readonly uuid: UUID;
-};
-
-export type ResourcePack = {
-  readonly name: string;
-  readonly description: string;
-  readonly uuid: UUID;
-  readonly module_uuid: UUID;
 };
 
 export type AddonHeader = {
@@ -34,15 +18,16 @@ export type AddonHeader = {
   readonly description: string;
   readonly version: SemVer;
   readonly min_engine_version: EngineVersion;
-  readonly uuid: UUID;
 };
 
 export type AddonMetadata = {
   readonly authors: string[];
+  readonly url: string;
+  readonly license: string;
 };
 
 export type RequiredAddons = {
-  readonly [addonId: string]: string; // id: version
+  readonly [addonId: string]: string;
 };
 
 export type SupportedTag = "official" | "approved" | "stable" | "experimental";
@@ -51,8 +36,6 @@ export type KairoAddonProperties = {
   readonly id: string;
   readonly metadata: AddonMetadata;
   readonly header: AddonHeader;
-  readonly resourcepack: ResourcePack;
-  readonly modules: ScriptModule[];
   readonly dependencies: ManifestDependency[];
   readonly requiredAddons: RequiredAddons;
   readonly tags: SupportedTag[];
